edbd5643c3b92bc9a694878643ffe83d5b048c64
Dante1930 <sahilbhatia220@gmail.com>
2023-03-30 18:43:37 +0530

    first commit

---
+++ react/src/views/SurveyView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PageComponent from '../components/PageComponent'
+
+export default function SurveyView() {
+  return (
+  <PageComponent title="Create New Survey">SurveyView</PageComponent>
+
+  )
+}
