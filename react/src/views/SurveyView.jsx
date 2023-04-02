4e6df5b7a06989cccce56ff6bba300a4065d136e
unknown <sahilbhatia22@gmail.com>
2023-04-03 03:38:23 +0530

    react on another

--- react/src/views/SurveyView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PageComponent from '../components/PageComponent'
-
-export default function SurveyView() {
-  return (
-  <PageComponent title="Create New Survey">SurveyView</PageComponent>
-
-  )
-}
